9f427205399febed04cd17a52e3ecf0670a4db6f
peter chege <peterchege442@gmail.com>
2020-06-04 22:57:46 +0300

    landing page completed

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Landingpage from '../views/Landingpage.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Landingpage',
+        component: Landingpage
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: () =>
+            import ('../views/Register.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
