f3c9135635ef419724a9841bf8e1d102f38f6c8e
peter chege <peterchege442@gmail.com>
2020-06-06 22:36:05 +0300

    test case passed

---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="counter">
+      counter: {{counter}}
+    </div>
+    <button @click="counter++">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    counter: 0
+  })
+}
+</script>
