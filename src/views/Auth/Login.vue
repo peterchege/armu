841df7b10cdde29baceb9bfc9cffed609e14233f
peter chege <peterchege442@gmail.com>
2020-06-08 20:23:23 +0300

    loginpage structure

---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-app id="inspire" class="image">
+        <v-container class="fill-height" max-width="600">
+            <app-logo/>
+            
+        </v-container>
+    </v-app>
+</template>
+
+<script>
+import HeaderLogo from '@/components/Header'
+export default {
+    components:{
+        appLogo: HeaderLogo,
+    }
+}
+</script>
+
+<style scoped>
+    .image {
+        background-image: url('../../assets/background.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+    .login-background {
+        background-image: url('../../assets/login-background.png');
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+  
+</style>
