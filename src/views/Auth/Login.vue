6e85de85acb76830d3ae39559459931281707a7f
peter chege <peterchege442@gmail.com>
2020-06-08 20:44:54 +0300

    avatar added

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -3,14 +3,14 @@
         <v-container class="fill-height" max-width="600">
             <app-logo/>
 
-            <v-content class="mx-auto">
+            <v-content class="mx-auto pb-4">
                 <v-row align="center" justify="center" class="text-center">
                     <v-col class="login-background pa-10 " cols="12" sm="12" md="4" >
-                        <span class="title ma-auto white--text py-10"> 
+                        <span class="title ma-auto white--text py-12"> 
                             Dont have an account ?
                         </span>
                          <v-spacer></v-spacer>
-                        <span class="caption white--text">
+                        <span class="caption white--text ">
                             Register here today to start trading
                         </span>
                         <v-content class="ma-7">
@@ -30,6 +30,18 @@
                     
                 </v-row>
             </v-content>
+
+            <v-row align="center" justify="end" >
+                <v-col  sm="12" md="4" align="right" >
+                    <v-img
+                    src="@/assets/avatars/buyers.png"
+                    max-width="250"
+                    max-height="250"
+                    class="">
+                    </v-img>
+                </v-col>
+               
+            </v-row>
 
         </v-container>
     </v-app>
