a5cb0d8354f17961c6f25cae9199b59557ecb2b1
peter chege <peterchege442@gmail.com>
2020-09-23 19:41:00 +0300

    dashboard intergration with side bar

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app id="inspire">
+    <app-side-bar/>
+    <!-- <app-header-menu/> -->
+
+    <v-content class="accent">
+      <v-container class="mx-3" align="center" justify="center" fill-height>
+              
+              
+        
+      </v-container>
+    </v-content>
+    
+    
+  </v-app>
+</template>
+
+<script>
+
+  export default {
+    components:{
+     appSideBar:() => import ('@/components/navigation/NavigationDrawer'),
+    //  appHeaderMenu:() => import ('@/components/navigation/HeaderMenu'),
+
+    },
+    props: {
+      source: String,
+    },
+    data: () => ({
+      dialog: false,
+      drawer: null,
+    }),
+    
+  }
+</script>
