6d8f761bfcf306ada1fd81385e5bc468a173c808
peter chege <peterchege442@gmail.com>
2020-11-03 23:52:51 +0300

    clean up

--- src/components/table/invoiceTable.vue
+++ src/components/table/invoiceTable.vue
@@ -38,67 +38,7 @@
                 </v-card-title>
             </v-col>
       </v-row>
-        <v-dialog v-model="dialog" max-width="700px">
-                    
-          <v-card >
-            <v-card-title class="primary darken-2 white--text justify-center mb-5 ">
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field  v-model="editedItem.id" label="id"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field disabled v-model="editedItem.make" label=" vehicle Make"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field disabled v-model="editedItem.model" label="Vehicle Model"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field disabled v-model="editedItem.yom" label="Year Of Manufacture"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field disabled v-model="editedItem.enginecc" label="Engine cc"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field disabled
-                      v-model="editedItem.regno" 
-                      label="Registration No.">
-                    </v-text-field>
-                  </v-col>
-                   
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn rounded
-                class="ma-5 pa-4" 
-                color="red" 
-                dark  
-                @click="close">
-                Cancel
-                <v-icon dark right>
-                    mdi-cancel
-                    </v-icon>
-                </v-btn>
-              <v-btn rounded
-                class="ma-5  pa-4" 
-                color="secondary" 
-                dark  
-                @click="save">
-                Save
-                 <v-icon dark right>
-                  mdi-checkbox-marked-circle
-                  </v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+       
     </template>
     <template v-slot:[`item.actions`]="{ item }">
 
@@ -131,52 +71,31 @@ import  { mapState,mapGetters } from 'vuex'
           text: 'Invoice No.',
           align: 'start',
           sortable: true,
-          value: 'id',
+          value: '',
         },
         {
           text: 'Customer Name',
           align: 'start',
           sortable: true,
-          value: 'make',
+          value: '',
         },
         {
-          text: 'Customer Reg No.',
+          text: 'Product',
           align: 'start',
           sortable: true,
-          value: 'model',
+          value: '',
         },
-        { text: 'Vendor Reg No.', value: 'yom' },
-         { text: 'Amount.', value: 'yom' },
-        { text: 'Created', value: 'engine_cc' },
-        // { text: 'Status', value: 'vendors.0.vendor_vehicle.vehicle_status' },
+        { text: 'No. of Bags', value: '' },
+         { text: 'Total Amount.', value: '' },
+        { text: 'Created', value: '' },
         { text: 'Actions', value: 'actions', sortable: false },
       ],
-      editedIndex: -1,
-      editedItem: {
-        id: '',
-        make: '',
-        model: '',
-        yom: '',
-        enginecc: '',
-        regno:'',
-        vehicleStatusId: '',
-  
-      },
-      defaultItem: {
-        id: '',
-        make: '',
-        model: '',
-        yom: '',
-        enginecc: '',
-        vehicleStatusId: ''
-      },
+
      
     }),
 
     computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'New Vehicle' : 'Edit Vehicle'
-      },
+     
       ...mapState(['fleet','loadingText']),
       ...mapGetters(['loadingTable'])
     },
@@ -204,27 +123,6 @@ import  { mapState,mapGetters } from 'vuex'
         this.dialog = true
       },
 
-      deleteItem (item) {
-        const index = this.fleet.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.fleet.splice(index, 1)
-      },
-
-      close () {
-        this.dialog = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.fleet[this.editedIndex], this.editedItem)
-        } else {
-          this.fleet.push(this.editedItem)
-        }
-        this.close()
-      },
     },
   }
 </script>
