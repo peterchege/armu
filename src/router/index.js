a7f8c3c63c75b7d946518a7afd8e5adb07061706
peter chege <peterchege442@gmail.com>
2020-11-09 23:39:24 +0300

    renaming component

--- src/router/index.js
+++ src/router/index.js
@@ -57,7 +57,7 @@ const routes = [{
         path: '/invoice',
         name: 'Invoice',
         component: () =>
-            import ('@/views/Invoice')
+            import ('@/views/InvoiceManagment')
     }
 
 ]
