e39b37b64e13ae236a3a1c165417ad1815a8625d
peter chege <peterchege442@gmail.com>
2021-02-18 18:22:35 +0300

    initializer and role based auth

--- src/components/navigation/NavigationDrawer.vue
+++ src/components/navigation/NavigationDrawer.vue
@@ -11,20 +11,23 @@
       </v-img>
       <v-divider/>
       <v-list-item-group color="secondary" class="my-5">
+        
+      <template v-for="(item, i) in items">
         <v-list-item
-          v-for="(item, i) in items"
-          :key="i" 
           :to="item.link"
           active-class="nav-active"
-          :v-if="item.position === role"
+          :key="i"
+          v-show="item.position === role"
         >
+      
           <v-list-item-icon>
             <v-icon v-text="item.icon"></v-icon>
           </v-list-item-icon>
-          <v-list-item-content>
+          <v-list-item-content >
             <v-list-item-title small v-text="item.text"></v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+      </template>
         
       </v-list-item-group>
     </v-list>
